eb9fc8198cb164f5aac40c23e1c239f35c719ac6
No-Biggie805 <56346454+No-Biggie805@users.noreply.github.com>
2024-01-10 22:20:26 +0000

    testes

---
+++ Threads_WIP/th1.1_experimental.c
@@ -0,0 +1,47 @@
+#include "string.h"
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <time.h>
+#include <unistd.h>
+
+#define TAM 10
+struct timespec ts;
+
+typedef struct Dados {
+  int Running;
+} Dados_t;
+
+//esta funcao tem como objectivo simular um sensor de temperatura..
+void *Sensor1(void *input) {
+  ts.tv_sec = 1;
+  ts.tv_nsec = 0;
+  while (((Dados_t*)input)->Running)
+  {
+      int random = rand() % 40;
+      printf("%d\n", random);
+      nanosleep(&ts, NULL);
+  }
+  return NULL;
+}
+
+int main(int argc, char *argv[]) {
+  
+  srand(time(NULL));
+  Dados_t *getDados =
+      (Dados_t *)malloc(sizeof(Dados_t) * 10); // alocacao dinamica
+
+  // create thread
+  pthread_t th1_1;
+  
+  int Running = 1;
+  getDados->Running = Running;
+  if (pthread_create(&th1_1, NULL, Sensor1, (void *)getDados) != 0)
+    return 1;
+
+  // Wait for the user input thread to finish
+  if(pthread_join(th1_1, NULL) != 0) return 2;
+  
+  return 0;
+}
--- UnixProcesses_WIP/main.c
+++ UnixProcesses_WIP/main.c
@@ -2,118 +2,162 @@
 Fazer processo principal;
 fazer processo filho 1, filho 2
 
-Simulacao de leitura de dados realistas dos sensores de temperatura ee luminosidade, pode recorrer ao rand();
-Aplicar logicas especificas de threads, para cada thread, baseadass nos valores dos sensores
+Simulacao de leitura de dados realistas dos sensores de temperatura ee
+luminosidade, pode recorrer ao rand(); Aplicar logicas especificas de threads,
+para cada thread, baseadass nos valores dos sensores
 
 */
+#include "string.h"
+#include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include "string.h"
-#include <time.h>
-#include <pthread.h>
-#include <unistd.h>
 #include <sys/wait.h>
+#include <time.h>
+#include <unistd.h>
 
-int generateRandomNumbers(pthread_t userInputThread, int Running);
-void *getUserInput(int Running);
+// int Running = 1;
+// int generateRandomNumbers(pthread_t userInputThread, int Running);
+// void *getUserInput();
 
-int main(int argc, char *argv[])
-{
-  // Parte de gerar numeros:
-  pthread_t userInputThread; // valor do tipo pthread_t, da libraria pthread!!
-  int Running = 1;
-
-  int res = generateRandomNumbers(userInputThread, Running);
-
-  if (res == 0)
-    // Wait for the user input thread to finish
-    pthread_join(userInputThread, NULL);
-  // Fim de gerador de numeros.
-
+int main() {
   // Inicio dos Processos
-  //  int pid1, pid2; // Identificador do processo
+  int pid1, pid2; // Identificador do processo
 
-  // pid1 = fork(); // Processo 1
+  pid1 = fork(); // Processo 1
 
-  // int getPid1 = 0, getPid2 = 0;
-  // if (pid1 < 0) // Se o fork() retornou erro
-  // {
-  //   perror(" Error: ");
-  //   return -1;
-  // }
-
-  // if (pid1 == 0) // if in child1 process
-  // {
-  //   getPid1 = getpid();
-  //   printf(" Hello from child1\t");
-  //   printf("getPid1: %d\n", getPid1);
-  //   exit(0);
-  // }
-  // else // PARENT
-  // {
-  //   pid2 = fork();
-  //   if (pid2 < 0) // Se o fork() retornou erro
-  //   {
-  //     perror(" Error: ");
-  //     return -1;
-  //   }
-  //   if (pid2 == 0)
-  //   {
-  //     getPid2 = getpid();
-  //     printf(" Hello from child2\t");
-  //     printf("getPid2: %d\n", getPid2);
-  //     exit(0);
-  //   }
-  //   //   int getPid = getppid ();
-  //   waitpid(pid1, NULL, 0);
-  //   waitpid(pid2, NULL, 0);
-  //   printf(" hello from parent \n ");
-  //   printf(" child1: %d\t\t child2: %d\n", pid1, pid2);
-  //   printf(" Exit \n");
-  // }
-  return 0;
-}
-
-int generateRandomNumbers(pthread_t userInputThread, int Running)
-{
-  struct timespec ts;
-
-  // Parte de gerar numeros:
-  ts.tv_sec = 1;
-  ts.tv_nsec = 0;
-
-  srand(time(NULL));
-
-  // Create a thread for user input
-  // pthread_t userInputThread;                                                    // valor do tipo pthread_t, da libraria pthread!!
-  if (pthread_create(&userInputThread, NULL, getUserInput(Running), NULL) != 0) // if the pthread_create() function returns 0, the thread was created successfully,
-  {                                                                             // otherwise, an error occurred
-    fprintf(stderr, "Error creating thread\n");
-    return 1;
+  int getPid1 = 0, getPid2 = 0;
+  if (pid1 < 0) // Se o fork() retornou erro
+  {
+    perror(" Error: ");
+    return -1;
   }
 
-  while (Running) // If the user writes, 'q' on the string, the getUserInput function will proceed and the Running flag will be set to 0
+  if (pid1 == 0) // if in child1 process
   {
-    int random = rand() % 40;
-    printf("%d\n", random);
-    nanosleep(&ts, NULL);
-  }
-
-  return 0;
-}
-
-
-void *getUserInput(int Running)
-{
-  char input[10];
-  while (Running)
+    getPid1 = getpid();
+    printf(" Hello from child1\t");
+    printf("getPid1: %d\n", getPid1);
+    exit(0);
+  } else // PARENT
   {
-    if (fgets(input, sizeof(input), stdin) != NULL) // fgets(char *s, int size, FILE *stream), tem a stream de informacao(tamanho sera da palavra, no caso "q"), o tamanho dessa informacao, e o apontador para a stream
-                                                    // Se o fgets da informacao for diferente de NULL, inicia a comparacao e se for igual a "q", o programa ativa a flag de saida
+    pid2 = fork();
+    if (pid2 < 0) // Se o fork() retornou erro
     {
-      if (strcmp(input, "q\n") == 0)
-        Running = 0;
+      perror(" Error: ");
+      return -1;
     }
+    if (pid2 == 0) {
+      getPid2 = getpid();
+      printf(" Hello from child2\t");
+      printf("getPid2: %d\n", getPid2);
+      exit(0);
+    }
+    //   int getPid = getppid ();
+    waitpid(pid1, NULL, 0);
+    waitpid(pid2, NULL, 0);
+    printf(" hello from parent \n ");
+    printf(" child1: %d\t\t child2: %d\n", pid1, pid2);
+    printf(" Exit \n");
   }
-  return NULL; // return argument, as getUserInput()
+  return 0;
 }
+// void *getUserInput(void *arg) {
+//   char input;
+//   while (Running) {
+//     input = getc(stdin);
+//     if (input != EOF) {
+//       if (input == 'q' || input == 'Q')
+//         Running = 0;
+//     }
+//   }
+//   return NULL;
+// }
+
+// int main() {
+//   srand(time(NULL));
+
+//   // Create a thread for user input
+//   pthread_t userInputThread;
+//   if (pthread_create(&userInputThread, NULL, getUserInput, NULL) != 0) {
+//     fprintf(stderr, "Error creating thread\n");
+//     return 1;
+//   }
+
+//   while (Running) {
+//     int random = rand() % 40;
+//     printf("%d\n", random);
+//     sleep(1);
+//   }
+
+//   // Wait for the user input thread to finish
+//   pthread_join(userInputThread, NULL);
+
+//   return 0;
+// }
+// int main(int argc, char *argv[])
+// {
+//   // Parte de gerar numeros:
+//   pthread_t userInputThread; // valor do tipo pthread_t, da libraria
+//   pthread!! int Running = 1;
+
+//   int res = generateRandomNumbers(userInputThread, Running);
+
+//   if (res == 0)
+//     // Wait for the user input thread to finish
+//     pthread_join(userInputThread, NULL);
+//   // Fim de gerador de numeros.
+
+//   return 0;
+// }
+
+// int generateRandomNumbers(pthread_t userInputThread, int Running)
+// {
+//   struct timespec ts;
+
+//   // Parte de gerar numeros:
+//   ts.tv_sec = 1;
+//   ts.tv_nsec = 0;
+
+//   srand(time(NULL));
+
+//   // Create a thread for user input
+//   // pthread_t userInputThread; // valor do tipo pthread_t, da libraria
+//   pthread!! if (pthread_create(&userInputThread, NULL, getUserInput(Running),
+//   NULL) != 0) // if the pthread_create() function returns 0, the thread was
+//   created successfully, { // otherwise, an error occurred
+//     fprintf(stderr, "Error creating thread\n");
+//     return 1;
+//   }
+
+//   while (Running) // If the user writes, 'q' on the string, the getUserInput
+//   function will proceed and the Running flag will be set to 0
+//   {
+//     int random = rand() % 40;
+//     printf("%d\n", random);
+//     nanosleep(&ts, NULL);
+//   }
+
+//   return 0;
+// }
+
+// void *getUserInput(int Running)
+// {
+//   char input[10];
+//   while (Running)
+//   {
+//     if (fgets(input, sizeof(input), stdin) != NULL) // fgets(char *s, int
+//     size, FILE *stream), tem a stream de informacao(tamanho sera da palavra,
+//     no caso "q"), o tamanho dessa informacao, e o apontador para a stream
+//                                                     // Se o fgets da
+//                                                     informacao for diferente
+//                                                     de NULL, inicia a
+//                                                     comparacao e se for igual
+//                                                     a "q", o programa ativa a
+//                                                     flag de saida
+//     {
+//       if (strcmp(input, "q\n") == 0)
+//         Running = 0;
+//     }
+//   }
+//   return NULL; // return argument, as getUserInput()
+// }
