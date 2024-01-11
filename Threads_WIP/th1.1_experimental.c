17c4a6792b2eca93d35963245e8428209ea3c33d
No-Biggie805 <56346454+No-Biggie805@users.noreply.github.com>
2024-01-11 22:37:17 +0000

    inicio de adição do tamnho do buffer, mais limite de adição dos valores

--- Threads_WIP/th1.1_experimental.c
+++ Threads_WIP/th1.1_experimental.c
@@ -8,33 +8,41 @@
 
 #define TAM 10
 struct timespec ts;
+int array[TAM];
 
 typedef struct Dados {
   int Running;
 } Dados_t;
 
 //esta funcao tem como objectivo simular um sensor de temperatura..
-void *Sensor1(void *input) {
+void *Sensor1(void *input) 
+{
   ts.tv_sec = 1;
   ts.tv_nsec = 0;
+  int i = 0;
+
   while (((Dados_t*)input)->Running)
   {
+      i = i+1;
       int random = rand() % 40;
-      printf("%d\n", random);
+      array[i] = random;
+      printf("%u\t, %d\n", random,array[i]);
       nanosleep(&ts, NULL);
-  }
+      
+  if(i == 10) //fazer condiçao se passar de n valores
+  ((Dados_t*)input)->Running = 0;
+  }  
   return NULL;
 }
 
 int main(int argc, char *argv[]) {
   
   srand(time(NULL));
-  Dados_t *getDados =
-      (Dados_t *)malloc(sizeof(Dados_t) * 10); // alocacao dinamica
+  Dados_t *getDados = (Dados_t *)malloc(sizeof(Dados_t) * 10); // alocacao dinamica
 
   // create thread
   pthread_t th1_1;
-  
+
   int Running = 1;
   getDados->Running = Running;
   if (pthread_create(&th1_1, NULL, Sensor1, (void *)getDados) != 0)
